1e55405e0182f313da7d378483c4481b60629f69
fingerQin <753814253@qq.com>
2020-03-26 10:37:10 +0800

    异常增加与优化

--- src/Exception/FingerException.php
+++ src/Exception/FingerException.php
@@ -8,7 +8,53 @@
 
 namespace finger\Exception;
 
+use finger\Ip;
+use finger\Url;
+
 class FingerException extends \Exception
 {
-    
+    /**
+     * 错误信息以数据返回。
+     * 
+     * -- 通常用于特殊环境解析 JSON 存入日志系统用。
+     *
+     * @return array
+     */
+    public function __toArray()
+    {
+        $serverIP   = isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : '127.0.0.1';
+        $clientIP   = Ip::ip();
+        $requestUrl = Url::getUrl();
+        $datetime   = date('Y-m-d H:i:s', time());
+        $errors = [
+            'err_time'    => $datetime,
+            'err_msg'     => $this->message,
+            'err_code'    => $this->code,
+            'service_ip'  => $serverIP,
+            'client_ip'   => $clientIP,
+            'req_url'     => $requestUrl,
+            'stack_trace' => $this->getTraceAsString()
+        ];
+        return $errors;
+    }
+
+    /**
+     * 重写 __toString()。
+     * @return string
+     */
+    public function __toString()
+    {
+        $serverIP   = isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : '127.0.0.1';
+        $clientIP   = Ip::ip();
+        $requestUrl = Url::getUrl();
+        $datetime   = date('Y-m-d H:i:s', time());
+        $errLog     = "err_time:{$datetime} \n";
+        $errLog    .= "err_msg: {$this->message} \n";
+        $errLog    .= "err_code: [{$this->code}] \n";
+        $errLog    .= "server_ip: [{$serverIP}] \n";
+        $errLog    .= "client_ip: [{$clientIP}] \n";
+        $errLog    .= "req_url: [{$requestUrl}] \n";
+        $errLog    .= "stack_trace:\n" . $this->getTraceAsString();
+        return $errLog;
+    }
 }
--- src/Exception/ServiceException.php
+++ src/Exception/ServiceException.php
@@ -36,6 +36,33 @@ class ServiceException extends FingerException
     }
 
     /**
+     * 错误信息以数据返回。
+     * 
+     * -- 通常用于特殊环境解析 JSON 存入日志系统用。
+     *
+     * @return array
+     */
+    public function __toArray()
+    {
+        $serverIP   = isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : '127.0.0.1';
+        $clientIP   = Ip::ip();
+        $requestUrl = Url::getUrl();
+        $datetime   = date('Y-m-d H:i:s', time());
+        $errors = [
+            'err_time'    => $datetime,
+            'err_msg'     => $this->message,
+            'err_code'    => $this->code,
+            'server_ip'   => $serverIP,
+            'client_ip'   => $clientIP,
+            'req_url'     => $requestUrl,
+            'method'      => $this->classNameAndMethod,
+            'params'      => json_decode($this->methodArgs),
+            'stack_trace' => $this->getTraceAsString()
+        ];
+        return $errors;
+    }
+
+    /**
      * 重写 __toString()。
      * @return string
      */
@@ -45,15 +72,15 @@ class ServiceException extends FingerException
         $clientIP   = Ip::ip();
         $requestUrl = Url::getUrl();
         $datetime   = date('Y-m-d H:i:s', time());
-        $errLog     = "ErrorTime:{$datetime} \n";
-        $errLog    .= "ErrorMsg: {$this->message} \n";
-        $errLog    .= "ErrorCode: [{$this->code}] \n";
-        $errLog    .= "ServerIP: [{$serverIP}] \n";
-        $errLog    .= "ClientIP: [{$clientIP}] \n";
-        $errLog    .= "RequestUrl: [{$requestUrl}] \n";
-        $errLog    .= "Method: {$this->classNameAndMethod}\n";
-        $errLog    .= "Params:\n" . print_r($this->methodArgs, true) . "\n";
-        $errLog    .= "StackTrace:\n" . $this->getTraceAsString();
+        $errLog     = "err_time:{$datetime} \n";
+        $errLog    .= "err_msg: {$this->message} \n";
+        $errLog    .= "err_code: [{$this->code}] \n";
+        $errLog    .= "server_ip: [{$serverIP}] \n";
+        $errLog    .= "client_ip: [{$clientIP}] \n";
+        $errLog    .= "req_url: [{$requestUrl}] \n";
+        $errLog    .= "method: {$this->classNameAndMethod}\n";
+        $errLog    .= "params:\n" . print_r($this->methodArgs, true) . "\n";
+        $errLog    .= "stack_trace:\n" . $this->getTraceAsString();
         return $errLog;
     }
 }
