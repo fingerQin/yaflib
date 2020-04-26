d8288c66322a8c6e06c09b2ff086987bd8f24e03
fingerQin <753814253@qq.com>
2020-04-26 14:48:49 +0800

    增加一个分页方法 Paginator::pageShow() 适配 bootstrap 框架

--- src/Paginator.php
+++ src/Paginator.php
@@ -78,6 +78,99 @@ class Paginator
 
     /**
      * 组装分页链接
+     * 
+     * -- 适配 bootstrap css 框架。
+     *
+     * @return string
+     */
+    public function pageShow()
+    {
+        if (0 == $this->totalRows) {
+            return '';
+        }
+        /* 生成URL */
+        $this->parameter[$this->p] = '[PAGE]';
+        $this->url = Url::getCurrentTrimPageUrl($this->parameter);
+        /* 计算分页信息 */
+        $this->totalPages = ceil($this->totalRows / $this->listRows); // 总页数
+        if (!empty($this->totalPages) && $this->nowPage > $this->totalPages) {
+            $this->nowPage = $this->totalPages;
+        }
+        /* 计算分页临时变量 */
+        $nowCoolPage     = $this->rollPage / 2;
+        $nowCoolPageCeil = ceil($nowCoolPage);
+        $this->lastSuffix && $this->config['last'] = $this->totalPages;
+        // 上一页
+        $upRow  = $this->nowPage - 1;
+        $upPage = '<li class="disabled"><a aria-label="Previous" href="#"><span aria-hidden="true">' . $this->config['prev'] . '</span></a></li>';
+        if ($upPage > 0) {
+            $upPage = '<li><a aria-label="Previous" href="' . $this->url($upRow) . '"><span aria-hidden="true">' . $this->config['prev'] . '</span></a></li>';
+        }
+        // 下一页
+        $downRow  = $this->nowPage + 1;
+        $downPage = '<li class="disabled"><a aria-label="Next" href="#"><span aria-hidden="true">' . $this->config['next'] . '</span></a></li>';
+        if ($downRow <= $this->totalPages) {
+            $downPage = '<li><a aria-label="Next" href="' . $this->url($downRow) . '"><span aria-hidden="true">' . $this->config['next'] . '</span></a></li>';
+        }
+        // 第一页
+        $theFirst = '';
+        if ($this->totalPages > $this->rollPage && ($this->nowPage - $nowCoolPage) >= 1) {
+            $theFirst = '<li class="firstPage"><a aria-label="Previous" href="' . $this->url(1) . '"><span aria-hidden="true">' . $this->config['first'] . '</span></a></li>';
+        }
+        // 最后一页
+        $theEnd = '';
+        if ($this->totalPages > $this->rollPage && ($this->nowPage + $nowCoolPage) < $this->totalPages) {
+            $theFirst = '<li class="endPage"><a aria-label="Next" href="' . $this->url($this->totalPages) . '"><span aria-hidden="true">' . $this->config['last'] . '</span></a></li>';
+        }
+        // 数字链接
+        $linkPage = "";
+        for($i = 1; $i <= $this->rollPage; $i ++) {
+            if (($this->nowPage - $nowCoolPage) <= 0) {
+                $page = $i;
+            } elseif (($this->nowPage + $nowCoolPage - 1) >= $this->totalPages) {
+                $page = $this->totalPages - $this->rollPage + $i;
+            } else {
+                $page = $this->nowPage - $nowCoolPageCeil + $i;
+            }
+            if ($page > 0 && $page != $this->nowPage) {
+                if ($page <= $this->totalPages) {
+                    $linkPage .= '<li><a href="' . $this->url($page) . '">' . $page . '</a></li>';
+                } else {
+                    break;
+                }
+            } else {
+                if ($page > 0 && $this->totalPages != 1) {
+                    $linkPage .= '<li class="active"><a href="#"><span class="sr-only">' . $page . '</span></a></li>';
+                }
+            }
+        }
+        // 替换分页内容
+        $page_str = str_replace([
+            '%HEADER%',
+            '%NOW_PAGE%',
+            '%UP_PAGE%',
+            '%DOWN_PAGE%',
+            '%FIRST%',
+            '%LINK_PAGE%',
+            '%END%',
+            '%TOTAL_ROW%',
+            '%TOTAL_PAGE%'
+        ], [
+            $this->config['header'],
+            $this->nowPage,
+            $upPage,
+            $downPage,
+            $theFirst,
+            $linkPage,
+            $theEnd,
+            $this->totalRows,
+            $this->totalPages
+        ], $this->config['theme']);
+        return "<nav aria-label=\"Page navigation\"><ul class=\"pagination\">{$page_str}</ul></nav>";
+    }
+
+    /**
+     * 组装分页链接
      *
      * @return string
      */
